5253fd3f13aa573ad73a626165b1ec43071d09d8
Ashley Felton <ashley@ropable.com>
2014-01-15 02:50:06 +0000

    If/Else statement on install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm ~/.vimrc && rm ~/.vim
+if (rm ~/.vimrc && rm ~/.vim); then
+    echo 'Removed old .vim and .vimrc'
+fi
 ln -s "$DIR/vimrc" ~/.vimrc && ln -s "$DIR/vim" ~/.vim && ln -s "$DIR/bundle" ~/.vim/bundle
