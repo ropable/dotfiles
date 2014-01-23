a9d9ab513303e12badf0341a060b69ca451e8101
Ashley Felton <ashley@ropable.com>
2014-01-23 10:43:19 +0800

    Improve install.sh

--- bashrc
+++ bashrc
@@ -105,5 +105,6 @@ fi
 # Virtualenvwrapper settings
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/projects
+source /usr/local/bin/virtualenvwrapper.sh
 export PIP_VIRTUALENV_BASE=WORKON_HOME
 export PIP_DOWNLOAD_CACHE=$HOME/.pip_download_cache
--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm ~/.vimrc && rm ~/.vim
-ln -s "$DIR/vimrc" ~/.vimrc && ln -s "$DIR/vim" ~/.vim && ln -s "$DIR/bundle" ~/.vim/bundle
+rm ~/.bashrc
+ln -s "$DIR/bashrc" ~/.bashrc
+rm -r ~/.vim/bundle
+rm -r ~/.vim
+rm ~/.vimrc
+ln -s "$DIR/vimrc" ~/.vimrc
+ln -s "$DIR/vim" ~/.vim
+ln -s "$DIR/bundle" ~/.vim/bundle
+rm ~/.jshintrc && rm ~/.jshintignore
+ln -s "$DIR/jshintrc" ~/.jshintrc && ln -s "$DIR/jshintignore" ~/.jshintignore
+rm ~/.gitconfig
+ln -s "$DIR/gitconfig" ~/.gitconfig
