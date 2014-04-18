bb1272e3663f93661a7b2fcd0925f78188b956ec
Ashley Felton <ashley@ropable.com>
2014-04-18 22:12:13 +0800

    Removed all submodules, swapped out vimrc for using Janus instead.

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.bashrc
 ln -s "$DIR/bashrc" ~/.bashrc
-rm -r ~/.vim/bundle
-rm -r ~/.vim
-rm ~/.vimrc
-ln -s "$DIR/vimrc" ~/.vimrc
-ln -s "$DIR/vim-pathogen" ~/.vim
-ln -s "$DIR/bundle" ~/.vim/bundle
+# Switched out for using Janus instead of a bunch of Git submodules.
+#rm -r ~/.vim/bundle
+#rm -r ~/.vim
+#rm ~/.vimrc
+#ln -s "$DIR/vimrc" ~/.vimrc
+#ln -s "$DIR/vim-pathogen" ~/.vim
+#ln -s "$DIR/bundle" ~/.vim/bundle
+rm ~/.vimrc.before && rm ~/.vimrc.after
+ln -s "$DIR/vimrc.before" ~/.vimrc.before && ln -s "$DIR/vimrc.after" ~/.vimrc.after
 rm ~/.jshintrc && rm ~/.jshintignore
 ln -s "$DIR/jshintrc" ~/.jshintrc && ln -s "$DIR/jshintignore" ~/.jshintignore
 rm ~/.gitconfig
 ln -s "$DIR/gitconfig" ~/.gitconfig
+rm ~/.hgrc
+ln -s "$DIR/hgrc" ~/.hgrc
+# Install bash alias from alias.sh
+rm ~/.bash_aliases
+wget -q -O - "$@" https://alias.sh/user/603/alias >> ~/.bash_aliases
