0d03b441218cf2f4732bd941b068664368a08a21
Ashley Felton <ashley@ropable.com>
2014-05-01 11:44:14 +0800

    Extra steps in initial bootstrap.

--- bootstrap_new_install.sh
+++ bootstrap_new_install.sh
@@ -1,7 +1,8 @@
-# Install bare essentials.
 cd ~
-sudo apt-get update
-sudo apt-get install byobu git mercurial curl python-software-properties
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-sudo python get-pip.py
-sudo pip install virtualenv virtualenvwrapper
+# Install bare essentials.
+sudo apt-get update && sudo apt-get install byobu git mercurial curl python-software-properties
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py && sudo python get-pip.py && sudo pip install virtualenv virtualenvwrapper
+# Download bash aliases.
+wget -q -O - "$@" https://alias.sh/user/603/alias >> ~/.bash_aliases
+# Create .ssh directory and generate an RSA key.
+mcd .ssh && ssh-keygen -f id_rsa -t rsa -N ''
