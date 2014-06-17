45f5c2254bc903b1eb2d2b627640ade53750c8d3
Ashley Felton <ashley@ropable.com>
2014-06-17 21:07:26 +0800

    Tweaked boostrap_new_install.sh

--- bootstrap_new_install.sh
+++ bootstrap_new_install.sh
@@ -1,7 +1,7 @@
 # Install bare essentials.
 cd ~
 sudo apt-get update
-sudo apt-get install byobu git mercurial curl python-software-properties
+sudo apt-get install byobu git mercurial curl python-software-properties python-dev
 wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 sudo python get-pip.py
 sudo pip install virtualenv virtualenvwrapper
