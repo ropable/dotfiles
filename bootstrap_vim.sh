f25359fc422cc6718e6b65208452f1cb6e4d90da
Ashley Felton <ashley@ropable.com>
2014-08-16 20:56:08 +0800

    Added vim-flake8 plugin, settings.

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -1,4 +1,10 @@
 # Install Vim and such.
 cd ~
 sudo apt-get install vim rake
+# Install Janus.
 curl -Lo- https://bit.ly/janus-bootstrap | bash
+# Make a .janus directory
+mkdir ~/.janus
+# Install vim-flake8
+cd ~/.janus
+git clone git@github.com:nvie/vim-flake8.git
