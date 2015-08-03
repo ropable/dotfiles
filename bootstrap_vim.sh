e212cdc24f0dd63fdf438542ee36f36cd2c625ae
Ashley Felton <ashley@ropable.com>
2015-08-03 15:56:13 +0800

    Added rust.vim plugin to bootstrap_vim.sh.

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -5,6 +5,8 @@ sudo apt-get install vim rake
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 # Make a .janus directory
 mkdir ~/.janus
-# Install vim-flake8
 cd ~/.janus
+# Install vim-flake8
 git clone https://github.com/nvie/vim-flake8.git
+# Install rust.vim
+git clone https://github.com/rust-lang/rust.vim.git
