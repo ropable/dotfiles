23d2ac6e7691e9999d802f695c70ae878736bf41
Ashley Felton <ashley@ropable.com>
2015-11-04 21:41:51 +0800

    Bug!

--- bootstrap_new_install.sh
+++ bootstrap_new_install.sh
@@ -1,6 +1,6 @@
 # Install bare essentials.
 sudo apt-get update && sudo apt-get install byobu git mercurial curl python-software-properties python-dev rake python-flake8
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py && sudo python get-pip.py && sudo pip install virtualenv virtualenvwrapper
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py && sudo -H python get-pip.py && sudo pip install virtualenv virtualenvwrapper
 # Create symlinks
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.bashrc
@@ -20,4 +20,4 @@ rm ~/.pypirc
 ln -s "$DIR/pypirc" ~/.pypirc
 # Create .ssh directory and generate an RSA key.
 cd ~
-mkdir -P .ssh && cd .ssh && ssh-keygen -f id_rsa -t rsa -N ''
+mkdir -p .ssh && cd .ssh && ssh-keygen -f id_rsa -t rsa -N ''
