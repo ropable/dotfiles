a7315a62d21268ca2f8eddf03d2340501b1bd320
Ashley Felton <ashley@ropable.com>
2015-12-23 13:50:59 +0800

    Remove gen SSH key.

--- bootstrap_new_install.sh
+++ bootstrap_new_install.sh
@@ -1,6 +1,5 @@
 # Install bare essentials.
 sudo apt-get update && sudo apt-get install byobu git mercurial curl python-software-properties python-dev rake python-flake8
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py && sudo -H python get-pip.py && sudo pip install virtualenv virtualenvwrapper
 # Create symlinks
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.bashrc
@@ -18,6 +17,6 @@ rm ~/.hgrc
 ln -s "$DIR/hgrc" ~/.hgrc
 rm ~/.pypirc
 ln -s "$DIR/pypirc" ~/.pypirc
-# Create .ssh directory and generate an RSA key.
+# Install Python tools
 cd ~
-mkdir -p .ssh && cd .ssh && ssh-keygen -f id_rsa -t rsa -N ''
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py && sudo -H python get-pip.py && sudo pip install virtualenv virtualenvwrapper
