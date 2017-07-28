f6bf35091864bac59eb1e3db2beb4469234845b6
Ashley Felton <ashley@ropable.com>
2017-07-28 08:15:39 +0800

    Added vim_extras.sh

---
+++ vim_extras.sh
@@ -0,0 +1,5 @@
+# Install some extra Vim plugins
+cd ~/.janus
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/alvan/vim-closetag.git
+git clone https://github.com/jiangmiao/auto-pairs.git
