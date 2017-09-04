12a4d5e03692c4435bcb62a51b5f9ce248f58d82
Ashley Felton <ashley@ropable.com>
2017-09-04 12:46:34 +0800

    Added more vim libraries. Change default tabstop for JavaScript.

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -12,3 +12,9 @@ git clone https://github.com/nvie/vim-flake8.git
 git clone https://github.com/rust-lang/rust.vim.git
 # Install jshint2.vim
 git clone https://github.com/Shutnik/jshint2.vim.git
+# vim-javascript
+git clone https://github.com/pangloss/vim-javascript.git
+# auto-pairs
+git clone https://github.com/jiangmiao/auto-pairs.git
+# vim-closetag
+git clone https://github.com/alvan/vim-closetag.git
