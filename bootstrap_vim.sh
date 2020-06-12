deb530c7becac5cf6a281e09368a2216ae927c80
Ashley Felton <ashley@ropable.com>
2020-06-12 09:12:16 +0800

    Symlink vimrc.after in Vim bootstrap.

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -1,3 +1,6 @@
+# Symlink vimrc.before and after to the home dir.
+ln -s vimrc.before ~/.vimrc.before
+ln -s vimrc.after ~/.vimrc.after
 # Install Vim and rake (for Janus)
 cd ~
 sudo apt-get install vim rake
