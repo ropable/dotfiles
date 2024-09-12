f0ed1827c69b25fddded1f4028770461e3cab02c
Ashley Felton <ashley@ropable.com>
2024-09-12 09:34:41 +0800

    Update bootstrap_python.sh

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -1,15 +1,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.pypirc
 ln -s "$DIR/pypirc" ~/.pypirc
-
-# Install Python tools (pyenv, Poetry)
 cd ~
-curl -Lo- https://pyenv.run | bash
-curl -sSL https://install.python-poetry.org | python3 -
 
-echo "Remember to set pyenv and Poetry config in .bashrc.local and .profile"
+# Install Python pyenv
+# https://github.com/pyenv/pyenv-installer
+curl https://pyenv.run | bash
+# Remember to set pyenv config in .bashrc.local and .profile
+# https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
 
-# References:
-# - https://github.com/pyenv/pyenv#basic-github-checkout
+# Install Poetry
+# https://python-poetry.org/docs/#installing-with-the-official-installer
 # - https://python-poetry.org/docs/managing-environments/
-
+# - https://python-poetry.org/docs/configuration/
+curl -sSL https://install.python-poetry.org | python3 -
+# Remember to set:
+# poetry config virtualenvs.prefer-active-python true
+# poetry config virtualenvs.in-project true
